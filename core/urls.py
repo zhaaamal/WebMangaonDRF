2ac4a26b54d4926180102a3d4be7e9eee6890520
zhaaamal <madiarovanurjamal@gmail.com>
2024-03-27 03:48:33 +0600

    add JWT

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,11 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 schema_view = get_schema_view(
@@ -21,5 +26,8 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('api/', include("app.urls"))
+    path('api/', include("app.urls")),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
